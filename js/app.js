10bc79f996972506ff5cfcbe2f33c0e1c03fe172
yechua-silva <yechua_silva@outlook.cl>
2022-12-15 17:50:26 -0300

    Agrege un email opcional, que no es necesario verificar

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     //Objeto con los datos ingresados
     const email = {
         email: '',
+        emailCC: '',
         asunto: '',
         mensaje: ''
     }
     
     //Seleccionar elemeentos de la interfaz
     const inputEmail = document.getElementById("email");
+    const inputEmailExtra = document.getElementById("emailCC");
     const inputAsunto = document.getElementById("asunto");
     const inputMensaje = document.getElementById("mensaje");
     const formulario = document.getElementById('formulario')
@@ -16,19 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnReset = document.querySelector('#formulario button[type="reset"]');
     const spinner = document.getElementById('spinner')
 
-    //Funciones
+    //FUNCIONES
     //Enviar email
     const enviarEmail = e => {
         e.preventDefault();
         //Mostrar spinner
         spinner.classList.add('flex');
         spinner.classList.remove('hidden');
-
         //Desaparecer spinner, sumular que ya se envio
         setTimeout(() => {
             spinner.classList.remove('flex');
             spinner.classList.add('hidden');
-
             //Reiniciamos el objeto
             resetFormulario()
             //Crear alerta
@@ -40,8 +40,9 @@ document.addEventListener("DOMContentLoaded", () => {
             //Desaparecer alerta
             setTimeout(() => {
                 alertaExito.remove();
+                limpiarAlerta(e.target.parentElement);
+                
             },2000)
-
         }, 2000);
     };
      //Validacon de eventos
@@ -63,10 +64,8 @@ document.addEventListener("DOMContentLoaded", () => {
         limpiarAlerta(e.target.parentElement);
         //Aignar valores
         email[e.target.id] = e.target.value.trim().toLowerCase();
-
         //Conprobar email
         comprobarEmail();
-
     }
     //Mostrar alertas de la validacion
     const mostrarAlerta = (mensaje, referencia) => {
@@ -98,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //Comprueba el email
     const comprobarEmail = () => {
         //Comprueba los campos vacios
-        if (Object.values(email).includes('')){
+        if (email.email === '' || email.asunto === '' || email.mensaje === '') {
             btnSubmit.classList.add('opacity-50');
             btnSubmit.disabled = true;
             return
@@ -111,20 +110,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const resetFormulario = () => {
         //Reiniciamos el objeto
         email.email = '';
+        email.emailCC = '';
         email.asunto = '';
         email.mensaje = '';
         formulario.reset();
         comprobarEmail();
     };
 
-
-
-
-    //Aignar eventos
+    const validarEmailExtra = e => {
+        //Comprueba si esta vacio y no generar la alerta
+        if (e.target.id === 'emailCC' && e.target.value === '') {
+            limpiarAlerta(e.target.parentElement);
+            return
+        }
+        //Verifica si el email es valido en caso de que lo agrege
+        if (e.target.id === 'emailCC' && !validarEmail(e.target.value)){
+            mostrarAlerta('El campo email no es valido', e.target.parentElement);
+            email[e.target.id] = '';
+            comprobarEmail()
+            return;  
+        }
+        //asignar valores
+        email[e.target.id] = e.target.value.trim().toLowerCase();
+        limpiarAlerta(e.target.parentElement);
+    };
+    //ASIGNACION DE EVENTOS
     //se ejecuta al salir del input
     inputEmail.addEventListener("input", validar)
     inputAsunto.addEventListener("input", validar)
     inputMensaje.addEventListener("input", validar)
+    inputEmailExtra.addEventListener("input", validarEmailExtra)
     formulario.addEventListener('submit', enviarEmail)
     btnReset.addEventListener('click', e => {
         // pare poder preguntar si esta seguro de resetear
