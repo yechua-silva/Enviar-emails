127685759eca38d8c1ba67a2dbd53c071ac69828
yechua-silva <yechua_silva@outlook.cl>
2022-12-15 14:25:14 -0300

    Inyecte un error, para la validacion y mostrar en el HTML

--- js/app.js
+++ js/app.js
@@ -5,21 +5,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputEmail = document.getElementById("email");
     const inputAsunto = document.getElementById("asunto");
     const inputMensaje = document.getElementById("mensaje");
+    const formulario = document.getElementById('formulario')
 
-    //Validaconl de eventos
+    //Funciones
+    //Mostrar alertas de la validacion
+    const mostrarAlerta = () => {
+        //Generar HTML
+        const error = document.createElement('P')
+        error.textContent = 'Hubo un error...';
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        
+        //Inyectar error al formulario
+        formulario.appendChild(error)
+    }
+
+
+    //Validacon de eventos
     const validar = e   => {
         //trim elimina espacios en blanco, siempre se recomienda ponerlo en un formulario
         if (e.target.value.trim() === '') {
-            console.log('Esta vacio');
+            mostrarAlerta();
         }else{
             console.log('Tiene contenido');
         }
     }
+
+
+
+
     //Aignar eventos
     //se ejecuta al salir del input
     inputEmail.addEventListener("blur", validar)
     inputAsunto.addEventListener("blur", validar)
     inputMensaje.addEventListener("blur", validar)
-
-
 })
