a61465fb7224d22acb0ee9028d7d373ec2371710
yechua-silva <yechua_silva@outlook.cl>
2022-12-15 13:38:30 -0300

    Selecione los elemento del interfaz

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    //Seleccionar elemeentos de la interfaz
+    const inputEmail = document.getElementById("email");
+    const inputAsunto = document.getElementById("asunto");
+    const inputMensaje = document.getElementById("mensaje");
+
+    console.log(inputMensaje, inputEmail, inputAsunto);
+})
